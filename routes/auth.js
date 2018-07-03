25031189b19a3ecfc2d0aeea8a0240db6711c408
Kingston Tam <web@ktam.org>
2018-07-03 18:06:24 +0200

    Add login screen

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 const models = require('../models');
 const Setting = models.Setting;
 
 let requiresEmptySettings = (req, res, next) => {
-  Setting.findAll().then(settings => {
+  Setting.findAll({attributes: ['id']}).then(settings => {
     if (settings.length > 0) {
       err = new Error("App has already been initialized");
       next(err);
@@ -31,30 +32,56 @@ router.post('/initialize', requiresEmptySettings, function(req, res, next) {
   }
 
   // Initialize settings
-  models.sequelize.transaction(t =>
-    Setting.create({
-      key: "username",
-      value: username
-    }, {transaction: t}).then(s =>
+  bcrypt.hash(password, 10).then((hash) =>
+    models.sequelize.transaction(t =>
       Setting.create({
-        key: "password",
-        value: password
-    }, {transaction: t})).then(s =>
-      Setting.create({
-        key: "website_url",
-        value: website_url
-    }, {transaction: t}))
+        key: "username",
+        value: username
+      }, {transaction: t}).then(s =>
+        Setting.create({
+          key: "password",
+          value: hash
+      }, {transaction: t})).then(s =>
+        Setting.create({
+          key: "website_url",
+          value: website_url
+      }, {transaction: t}))
+    )
   ).then(result => {
     res.redirect('/auth/login');
-  }).catch(err => {
+  }).catch((err) => {
     renderInitialize(res, "Error setting up database!");
-  })
-  console.log(req.body);
+  });
 });
+let renderLogin = (res, err = null) => {
+  res.render('login', { title: 'Log into Shortly', err: err});
+}
 
 /* GET login page */
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Log into Shortly' });
+  renderLogin(res);
+});
+
+/* POST login page */
+
+let checkLogin = (req, settings) => {
+  if (settings.get('username') !== req.body.username) return false;
+  return bcrypt.compare(req.body.password, settings.get('password'));
+}
+
+router.post('/login', function(req, res, next) {
+  Setting.findAll({attributes: ['key', 'value']}).then(rawSettings => {
+    let settings = new Map(rawSettings.map(i => [i.key, i.value]));
+    return checkLogin(req, settings);
+  }).then(isValid => {
+    if (isValid) {
+      res.redirect('/urls');
+    } else {
+      renderLogin(res, "Your username or password was incorrect.");
+    }
+  }).catch(err => {
+    renderLogin(res, "Unable to login");
+  })
 });
 
 module.exports = router;
