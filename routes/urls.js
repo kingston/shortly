edf15b082d2753c5ed36955534c1dd8a8b7f33d9
Kingston Tam <web@ktam.org>
2018-07-06 18:42:28 +0200

    Add upload support

--- routes/urls.js
+++ routes/urls.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const shortid = require('shortid');
+const fs = require('fs');
+const util = require('util');
 const models = require('../models');
 const authSession = require('../middleware/auth_session');
+
 
 const router = express.Router();
 const { Url } = models;
@@ -11,7 +16,6 @@ router.use('/urls', authSession.requiresLogin);
 // List URLs
 router.get('/urls', (req, res, next) => {
   Url.findAll().then((urls) => {
-    console.log(urls);
     const { err, success } = req.query;
     res.render('urls', { urls, err, success });
   }).catch((err) => {
@@ -22,6 +26,7 @@ router.get('/urls', (req, res, next) => {
 const invalidShorts = [
   'auth',
   'urls',
+  'public',
 ];
 
 const shortNameRegex = /[a-z0-9]*/i;
@@ -44,45 +49,77 @@ function formatUrl(url) {
   }
 }
 
+const uploadDirectory = `${__dirname}/../uploads`;
+const uploadOptions = {
+  safeFileNames: false,
+  abortOnLimit: true,
+  limits: { fileSize: 200 * 1024 * 1024 },
+};
+
 // Add new URL
-router.post('/urls/new', (req, res, next) => {
-  const {
-    short_name: shortName,
-    full_url: fullUrl,
-  } = req.body;
-  checkIsValidShortName(shortName).then((result) => {
-    if (!result) {
+router.post('/urls/new', fileUpload(uploadOptions), async (req, res, next) => {
+  let uploadedPath;
+  try {
+    const {
+      short_name: shortName,
+      full_url: fullUrl,
+    } = req.body;
+    if (!await checkIsValidShortName(shortName)) {
       res.redirect('/urls?err=Invalid%20short%20name');
       return;
     }
-    const parsedUrl = formatUrl(fullUrl);
-    if (!parsedUrl) {
-      res.redirect('/urls?err=Invalid%20url');
-      return;
+
+    const urlParams = { short_name: shortName };
+
+    if (req.files && req.files.attachment) {
+      const { attachment } = req.files;
+      const newFilename = shortid.generate();
+      try {
+        await util.promisify(fs.mkdir)(uploadDirectory);
+      } catch (ex) {
+        // ignore mkdir if already exists
+      }
+      uploadedPath = `${uploadDirectory}/${newFilename}`;
+      await attachment.mv(uploadedPath);
+      urlParams.file_location = newFilename;
+      urlParams.file_name = attachment.name;
+    } else {
+      urlParams.full_url = formatUrl(fullUrl);
+      if (!urlParams.full_url) {
+        res.redirect('/urls?err=Invalid%20url');
+        return;
+      }
     }
-    Url.create({
-      short_name: shortName,
-      full_url: parsedUrl,
-    }).then((url) => {
-      res.redirect('/urls?success=Successfully%20created!');
-    }).catch((err) => {
-      res.redirect('/urls?err=Error!');
-    });
-  }).catch((err) => {
-    res.redirect('/urls?err=Error!');
-  });
+    await Url.create(urlParams);
+    res.redirect('/urls?success=Successfully%20created!');
+  } catch (err) {
+    console.log(err);
+    if (uploadedPath) {
+      try {
+        await util.promisify(fs.unlink)(uploadedPath);
+      } catch (unlinkErr) {
+        // ignore
+      }
+    }
+    res.redirect(`/urls?err=${err.toString()}`);
+  }
 });
 
-router.post('/urls/delete/:id', (req, res, next) => {
-  Url.destroy({
-    where: {
-      id: req.params.id,
-    },
-  }).then((url) => {
+router.post('/urls/delete/:id', async (req, res, next) => {
+  try {
+    const url = await Url.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!url) throw new Error('Unable to find URL by ID');
+    await url.destroy();
+    await util.promisify(fs.unlink)(`${uploadDirectory}/${url.file_name}`);
     res.redirect('/urls?success=Deleted!');
-  }).catch((err) => {
+  } catch (err) {
+    console.log(err);
     res.redirect('/urls?err=Unable%20to%20delete!');
-  });
+  }
 });
 
 // Catch all
@@ -96,6 +133,11 @@ router.get('/:key?', (req, res, next) => {
   Url.findByShortName(key).then((url) => {
     if (url === null) {
       next(); // pass to 404
+    } else if (url.file_location) {
+      res.download(
+        `${uploadDirectory}/${url.file_location}`,
+        url.file_name,
+      );
     } else {
       res.redirect(url.full_url);
     }
