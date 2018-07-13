09586e056d2a1c7b966c3f221767ea8c0cefd7a5
Kingston Tam <web@ktam.org>
2018-07-14 01:18:52 +0300

    Add helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config.json')[env];
@@ -19,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
