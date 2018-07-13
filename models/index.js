32e8b853e48ed68890581dfc8eb5772d8d829665
Kingston Tam <web@ktam.org>
2018-07-14 00:34:21 +0300

    Fix operator error in Sequelize

--- models/index.js
+++ models/index.js
@@ -8,6 +8,10 @@ const db = {};
 const config = require('../config/config.json')[env];
 
 let sequelize;
+
+// disable operator aliases
+config.operatorsAliases = false;
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
